4a2f22d211fac9f76b1008cb7bd6fee208f1aaae
Fabricio Nogueira <nogsantos@gmail.com>
2015-09-17 09:44:59 -0300

    Initial Commit

---
+++ MinhaLocalizacao/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MinhaLocalizacao
+//
+//  Created by Fabricio Nogueira dos Santos on 9/17/15.
+//  Copyright © 2015 Fabricio Nogueira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MinhaLocalizacao/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MinhaLocalizacao
+//
+//  Created by Fabricio Nogueira dos Santos on 9/17/15.
+//  Copyright © 2015 Fabricio Nogueira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
